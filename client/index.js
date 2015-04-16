68d947577ddc5cef021f3279efb3f4b52ae42bce
Christian Richard <lifepupil@gmail.com>
2015-04-16 16:33:04 -0700

    Initial

--- client/index.js
+++ client/index.js
@@ -1,10 +1,23 @@
 'use strict';
 
 
-var root;
+var root, users;
 
 $(document).ready(init);
 
 function init(){
   root = new Firebase('https://zelda-cdr.firebaseio.com/');
+  users = root.child('users');
+  #('#create-user').click(createUser);
+}
+
+function createUser() {
+  var handle = $('#handle').val();
+  var avatar = $('#avatar').val();
+
+  users.push({
+    handle: handle,
+    avatar: avatar
+  });
+
 }
