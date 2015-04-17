3820c89440c4b392a84bf42e6a075f2ac4e39f99
Christian Richard <lifepupil@gmail.com>
2015-04-17 09:31:16 -0700

    updated 2

--- client/index.js
+++ client/index.js
@@ -1,27 +1,23 @@
-'use strict';
+/* global Firebase:true */
 
+'use strict';
 
 var root, characters;
 
 $(document).ready(init);
 
 function init(){
-  root = new Firebase('https://zelda-cdr.firebaseio.com/');
-  characters = root.child('users');
-
+  root = new Firebase('https://zelda-chyld.firebaseio.com/');
+  characters = root.child('characters');
   $('#create-user').click(createUser);
   $('#login-user').click(loginUser);
-
   characters.on('child_added', characterAdded);
   $('#create-character').click(createCharacter);
-
 }
 
-// THIS SENDS
-function createCharacter() {
+function createCharacter(){
   var handle = $('#handle').val();
   var avatar = $('#avatar').val();
-  // THIS GETS YOUR LOGIN INFO OUT OF localStorage (try typing it at the console...)
   var uid = root.getAuth().uid;
 
   characters.push({
@@ -31,47 +27,43 @@ function createCharacter() {
   });
 }
 
-// THIS GETS FIREBASE TO PUSH INFORMATION BACK TO ME (INCLUDING OTHER CHARACTERS THAT ARE PLAYING)
 function characterAdded(snapshot){
-   var character = snapshot.val();
-   var myUid = root.getAuth().uid;
-   var active = '';
+  var character = snapshot.val();
+  var myUid = root.getAuth().uid;
+  var active = '';
 
-   if (myUid === character.uid) {
-     myClass = 'active';
-   }
+  if(myUid === character.uid){
+    active = 'active';
+  }
 
-   var tr = '<tr class='+myClass+'><td>'+character.handle+'</td><td><img src"'+character.avatar+'"></td></tr>';
-   $('#characters > tbody').append(tr);
+  var tr = '<tr class="'+active+'"><td>'+character.handle+'</td><td><img src="'+character.avatar+'"></td></tr>';
+  $('#characters > tbody').append(tr);
 }
 
-function loginUser() {
+function loginUser(){
   var email = $('#email').val();
   var password = $('#password').val();
 
   root.authWithPassword({
     email    : email,
     password : password
-  }, function(error, authData) {
-    if (error) {
-      console.log("Login Failed with error:", error);
-    } else {
-      console.log("Authenticated successfully with payload:", authData);
+  }, function(error){
+    if(error){
+      console.log('Error logging in:', error);
     }
   });
 }
 
-function createUser() {
+function createUser(){
   var email = $('#email').val();
   var password = $('#password').val();
 
-  // SEE NOTES FOR EXPLANATION
   root.createUser({
     email    : email,
     password : password
-  }, function(error) {
-    if (error) {
-      console.log("Error creating user:", error);
+  }, function(error){
+    if(error){
+      console.log('Error creating user:', error);
     }
   });
 }
